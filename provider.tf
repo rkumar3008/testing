605675952249ffbee247a6ba8d12f3a2ca721748
rkumar3008 <2ranjeetkr@gmail.com>
2025-08-15 18:12:46 +0530

    modify resources

--- provider.tf
+++ provider.tf
@@ -20,12 +20,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rk-rg1" {
-    name = "us-rg"
+    name = "us-rg1"
     location = "east us"
 }
 
 resource "azurerm_storage_account" "us-stg" {
-    name = "usstg1"
+    name = "usstg12"
     resource_group_name = azurerm_resource_group.rk-rg1.name
     location = azurerm_resource_group.rk-rg1.location
     account_tier = "Standard"
